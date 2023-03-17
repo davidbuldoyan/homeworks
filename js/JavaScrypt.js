8188cc76be95f5d6ee5f1ac299e6ba60291c1206
David <david.buldoyan@gmail.com>
2023-03-17 19:17:36 +0400

    homework 9

--- js/JavaScrypt.js
+++ js/JavaScrypt.js
@@ -1,76 +1,60 @@
-//homework 8
-// console.log("1.Գեներացնել պատահական 5 թիվ 0-10 տիրույթում այնպես, որ 0-5 տիրույթի թվերը կլորանան դեպի վերև, իսկ մյուսները դեպի ներքև և արտածելպատահական թիվն ու կլորացված թիվը:");
-// alert("1.");
-// let randomNumber;
-// let roundedNumber;
-// for(let i = 0; i < 5; i++){
-//     randomNumber = Math.random() * 10;
-//     if(randomNumber < 5){
-//         roundedNumber = Math.ceil(randomNumber);
-//         console.log(`random number ${randomNumber}, rounded number ${roundedNumber}`);
-//     }else{
-//         roundedNumber = Math.floor(randomNumber);
-//         console.log(`random number ${randomNumber}, rounded number ${roundedNumber}`);
-//     }
-// }
+//homework 9
+console.log(`1.Ստեղծել Car կոնստրուկտոր ֆ-ա, որը ունի name, year, color, maxSpeed, ստեղծել bmw, mersedes օբյեկտները Car -ից:`);
+
+function Car(name, year, color, maxSpeed) {
+    this.name = name;
+    this.year = year;
+    this.color = color;
+    this.maxSpeed = maxSpeed;
+}
+
+ const bmw = new Car("i8", 2014, "black", 250);
+ const mersedes = new Car("w223", 2013, "black", 250);
+
+ console.log(bmw);
+ console.log(mersedes);
 
 
 
-// console.log("2.Գրել ֆունկցիա որը տրված բառի վերջին տառը կդարդձնի մեծատառ:");
-// alert("2.")
-// let word = prompt("enter the word");
-// function capitalizeLastLetter(word) {
-//     const index = word.length - 1;
-//     const lastIndex = word.charAt(index);
-//     const newWord = word.slice(0, index) + lastIndex.toUpperCase();
-//     return newWord;
-// }
-// console.log(capitalizeLastLetter(word));
+ console.log(`2.Ստեղծել մեթոդ String-ի համար: Այն ցանկացած բառի առաջին 2 տառը դարձնում է մեծատառ:`);
+
+ function StringMethof(word) {
+    this.word = word;
+    this.letterUpperCase = () => {
+        let word2 = word.split("");
+        let letter = word2[0].toUpperCase();
+        let letter2 = word2[1].toUpperCase();
+        return (letter + letter2 + word.slice(2));
+    };
+ }
+const word = new StringMethof("hello");
+console.log(word.letterUpperCase());
 
 
 
-// console.log(`3.Գրել ֆ-ա որը մուտքագրված բառից կարտածի միայն զույգ ինդեքսով տառերը: Օրինակ՝ ինդեքս -ից կմնա իդք:`);
-// let word2 = prompt("enter the word");
-// function zuygIndex(word2) {
-//     let arrIndexOfWord2 = [];
-//     for(let i = 0; i < word2.length; i++){
-//         if(i % 2 == 0){
-//             arrIndexOfWord2.push(word2.charAt(i));
-//         }
-//     }
-//     return arrIndexOfWord2;
-// }
-// console.log(zuygIndex(word2));
+console.log(`3.Գրել ծրագիր, որը կարտածի մինչև հունվարի 1 քանի օր է մնացել:`);
+const today = new Date();
+const nextYear = new Date(today.getFullYear() + 1, 0, 1);
+const timeDiff = nextYear.getTime() - today.getTime();
+const daysLeft = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+console.log(`hunvari 1-in mnacel e ${daysLeft} or`);
 
 
 
-// console.log(`4.Գրել ծրագիր, որը կգեներացնի 5 նիշանոց կոդ տառերից և թվերից կազմված, և եթե այցելուն prompt պատուհանում ճիշտ հավաքի այդ կոդը և հաստատի, ապա alert անի ճիշտ է, այլապես alert անի "սխալ է":`);
-// let code = '';
-// const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-// for (let i = 0; i < 5; i++) {
-//   code += characters.charAt(Math.floor(Math.random() * characters.length));
-// }
-// const input = prompt(`Enter the code: ${code}`);
-// if (input === code) {
-//   alert('Correct');
-// } else {
-//   alert('Incorrect');
-// }
-
-
-
-// console.log(`5.Ունենք 2 զանգված, գրել ծրագիր, որը կվերադարձնի 'Yes' եթե քիչ էլէմենտներով զանգվածը պարունակվում է մեծ զանգվածի մեջ և 'NO' հակառակ դեպքում: Օր. [9,9,8] և [8,9] վերադարձնում է 'YES'.`);
-let arrLong = [9, 9, 8];
-let arrSmall = [8, 1];
-for(let i = 0; i < arrSmall.length;){
-    for(let j = 0; j < arrLong.length; j++){
-        if(arrSmall[i] == arrLong[j]){
-            arrSmall.splice(i, 1)
-        }
+console.log(`4.Գրել ծրագիր, որը մուտքում կստանա տարի և ամիսների քանակ, ու կվերադարձնի քանի ամիս է մնացել կամ անցել տարեմուտից:`);
+function monthsPassedOrRemaining(year, month) {
+    const nowDays = new Date();
+    const target = new Date(year, month - 1, 1);
+    const months = (target.getFullYear() - nowDays.getFullYear()) * 12 + target.getMonth() - nowDays.getMonth();
+    if(months < 0){
+        return `ancel e ${Math.abs(months)} amis ${year} taruc`;
+    }else if(months > 0){   
+        return `mnacel e ${months} amis ${year} tarun`;
+    }else{
+        return `hima ays ${year} tarin ev ${month} amisn e`;
     }
 }
-if(arrSmall.length == 0){
-    console.log("Yes");
-}else{
-    console.log("No");
-}
+  
+  console.log(monthsPassedOrRemaining(2000, 6));
+  
